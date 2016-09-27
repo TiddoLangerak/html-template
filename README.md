d859a082b23b870607ccdda021dcb03e956baeb0
Tiddo Langerak <tiddolangerak@gmail.com>
2016-09-27 14:36:46 +0200

    Support for arrays, nesting & raw HTML

--- README.md
+++ README.md
@@ -7,8 +7,40 @@ import html from '@tiddo/html-template';
 
 const title = 'foo & bar';
 
-const output = html`<h1>${title}</h1>`; 
+const output = html`<h1>${title}</h1>`;
 
 output === "<h1>foo &amp; bar</h1>"
+```
+
+Note: You can also use array values. These will be flattened and concatenated.
+
+## Insert raw HTML
+```javascript
+import html, { unsafe } from '@tiddo/html-template';
+
+const embedded = '<b>Bold text</b>';
+const output = html`<p>${unsafe(embedded}}</p>`;
+output === '<p><b>Bold text</b></p>';
+```
+
+## Nesting HTML
+
+When nesting `html` templates you don't need to pass them through `unsafe`, that is already done for you:
+
+```javascript
+import html from '@tiddo/html-template';
+
+const items = [1,2,3];
+const list = html`
+	<ul>
+		${items.map(item => html`<li>{item}</li>`)}
+	</ul>
+`
+
+list === `
+	<ul>
+		<li>1</li><li>2</li><li>3</li>
+	</ul>
+`;
 ```
 
--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 import escape from 'escape-html';
 
+//Marker that can be placed on strings to indicate they shouldn't be escaped
+const _noescape = Symbol();
+
+//Gets a safe version of the input value:
+//- If the input is an array, it concatenates the safe version of it's elements
+//- If the input is a string marked with _noescape, it won't be escaped
+//- In all other cases the escaped string representation of the input is returned.
+function getSafe(value) {
+	if (Array.isArray(value)) {
+		return value.map(getSafe).join('');
+	}
+	if (typeof value === 'object' && value[_noescape] === _noescape) {
+		return value.toString();
+	} else {
+		return escape(value.toString());
+	}
+}
+
+//Template tag to create a HTML safe string.
 export default function html(parts, ...values) {
-	return parts
-		.map((part, i) => part + escape(values[i] || ''))
+	const html = parts
+		.map((part, i) => {
+			let safeValue = '';
+			if (values[i]) {
+				safeValue = getSafe(values[i]);
+			}
+			return part + safeValue;
+		})
 		.join('');
+
+	return unsafe(html); //It's already escaped, so we can mark it
+}
+
+//Marks a string to not be escaped
+export function unsafe(value) {
+	if (Array.isArray(value)) {
+		return value.map(unsafe);
+	}
+	const marked = new String(value.toString());
+	marked[_noescape] = _noescape;
+	return marked;
 }
